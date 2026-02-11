cd16c30070b94c5b05d725a50a4af935d6c412d0
BOXNYC <joe@box.biz>
2026-02-11 18:45:07 -0500

    Fixed document.body bug

--- frontend/app/components/BG.tsx
+++ frontend/app/components/BG.tsx
@@ -2,15 +2,24 @@
 
 import { getThemeFromPath } from "@/utils/theme";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 export default function BG() {
   const pathname = usePathname();
   const THEME = getThemeFromPath(pathname);
-  return (
-    createPortal(
-      <div className={`fixed inset-0 -z-10 ${THEME.BACKGROUND}`} />,
-      document.body
-    )
-  )
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={`fixed inset-0 -z-10 ${THEME.BACKGROUND}`} />,
+    document.body
+  );
 }
