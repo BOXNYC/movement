9c6094db845afc237f948b1797026166f0615d4b
BOXNYC <joe@box.biz>
2026-02-11 18:55:33 -0500

    fix

--- frontend/app/components/BG.tsx
+++ frontend/app/components/BG.tsx
@@ -2,24 +2,12 @@
 
 import { getThemeFromPath } from "@/utils/theme";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
 
 export default function BG() {
   const pathname = usePathname();
   const THEME = getThemeFromPath(pathname);
-  const [container, setContainer] = useState<HTMLElement | null>(null);
 
-  useEffect(() => {
-    setContainer(document.body);
-  }, []);
-
-  if (!container) {
-    return null;
-  }
-
-  return createPortal(
-    <div className={`fixed inset-0 -z-10 ${THEME.BACKGROUND}`} />,
-    container
+  return (
+    <div className={`fixed inset-0 -z-10 ${THEME.BACKGROUND}`} />
   );
 }
